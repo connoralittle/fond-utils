3bce7604b68ca4d02a58433cf9baa921f8d3bae7
ssardina <ssardina@gmail.com>
2025-04-28 09:57:18 +1000

    remove all imports before toml file #20

--- fondutils/__init__.py
+++ fondutils/__init__.py
@@ -9,13 +9,6 @@
 """Top-level package for fond-utils."""
 
 from .__version__ import (
-    __author__,
-    __author_email__,
-    __copyright__,
-    __description__,
-    __license__,
-    __title__,
-    __url__,
     __version__,
 )
 from .helpers.base import _get_current_path
